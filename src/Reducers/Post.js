096bc8e3adfe15bcef8b719056fd013aa2439db7
unknown <coolrass1@aol.co.uk>
2022-05-01 17:55:20 +0200

    tool212

--- src/Reducers/Post.js
+++ src/Reducers/Post.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import axios from "axios"
 
-const url="http://localhost:5000/posts"
+const url="http://mail.librahouse.co.uk:5000/posts"
 const initialState = {
   entities: [],
   loading: false,
